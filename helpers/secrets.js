a6b4af7f5d2f54371d89c7b9c88c64dc9bb6c548
Carlo Enrico <carlo.enrico@uol.com.br>
2019-07-29 19:24:07 -0300

    Update

---
+++ helpers/secrets.js
@@ -0,0 +1,3 @@
+export default {
+  key: '33e8fd4a453b4c9b37e209780920ef9f'
+}
--- modules/login/actions/userLogin.js
+++ modules/login/actions/userLogin.js
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken'
+import secret from '../../../helpers/secrets'
 
 export default (req, res) => {
   const { user, pass } = req.body
-  const token = jwt.sign('daasd', 'abdc')
+  const token = jwt.sign({ user }, secret.key, {
+    expiresIn: 3600
+  })
+  res.cookie('Authorization', token, { maxAge: 3600, httpOnly: true })
   res.send({ user, pass, token })
 }
